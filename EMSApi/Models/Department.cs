1d25da5784b1e8d5345a1a399dc9b487d59541c2
Iamneo <techadmin@iamneo.ai>
2023-11-06 09:13:49 +0000

    Backup Commit

---
+++ EMSApi/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMSApi.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string DeptName { get; set; } = null!;
+
+    public string? Location { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
+}
---
+++ EMSApi/Models/Dept1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMSApi.Models;
+
+public partial class Dept1
+{
+    public int? Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Location { get; set; }
+}
