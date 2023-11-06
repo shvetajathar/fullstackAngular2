d2f90dd60540edd7872c6c6e5a802d369cad4656
Iamneo <techadmin@iamneo.ai>
2023-11-06 10:58:57 +0000

    Backup Commit

--- EMSApi/Program.cs
+++ EMSApi/Program.cs
@@ -1,3 +1,5 @@
+using EMSApi.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.services.AddDbContext<EmsDbContext>(options=>options.UseSqlServer(configuration.GetConnectionString("mycon")));
 
 var app = builder.Build();
 
