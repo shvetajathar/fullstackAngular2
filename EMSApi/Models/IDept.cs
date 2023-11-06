c6e7c77faf0a4fd100d2d07ebdef14dbb961a6ba
Iamneo <techadmin@iamneo.ai>
2023-11-06 09:28:49 +0000

    Backup Commit

---
+++ EMSApi/Models/IDept.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+namespace EMSApi.Models;
+
+public interface IDept
+{
+    public void GetAll();
+    public void GetById();
+    public void GetAll();
+    public void GetAll();
+
+}
