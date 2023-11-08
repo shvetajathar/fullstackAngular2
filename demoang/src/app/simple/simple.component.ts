0cee988e9a949504dcb7d886564045c8050b4199
Iamneo <techadmin@iamneo.ai>
2023-11-08 10:22:13 +0000

    Backup Commit

---
+++ demoang/src/app/simple/simple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple',
+  templateUrl: './simple.component.html',
+  styleUrls: ['./simple.component.css']
+})
+export class SimpleComponent implements OnInit {
+  weekdays:string[]=['sunday','monday','tuesday','wednesday','thursday','friday']
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
