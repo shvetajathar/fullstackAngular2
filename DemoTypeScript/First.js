85295a0ff30992d43d52103e1e110421d90cd456
Iamneo <techadmin@iamneo.ai>
2023-11-08 05:00:33 +0000

    Backup Commit

---
+++ DemoTypeScript/First.js
@@ -0,0 +1,5 @@
+console.log("Welcome to typescript");
+var firstname = 'Tom';
+var age = 33;
+console.log("Name= " + firstname);
+console.log("Age= " + age);
---
+++ DemoTypeScript/First.ts
@@ -0,0 +1,12 @@
+console.log("Welcome to typescript")
+var firstname:string='Tom'
+var age:number=33
+
+console.log("Name= "+firstname)
+console.log("Age= "+age)
+
+var fno:number=70
+var sno:number=80
+var csno:string='40'
+var result=fno+csno+sno
+console.log(result)
