845b166e3abade7fe07e079f8230bca27a253129
Iamneo <techadmin@iamneo.ai>
2023-11-09 04:34:35 +0000

    Backup Commit

--- demoang/src/app/simple/simple.component.ts
+++ demoang/src/app/simple/simple.component.ts
@@ -10,6 +10,14 @@ export class SimpleComponent implements OnInit {
   weekdays:string[]=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
   age:number=26
   emp:Employee={id:123,name:'Tom',salary:40000,city:'Mumbai'}
+  emplist:Employee[]=[
+    {id:123,name:'Tom',salary:40000,city:'Mumbai'},
+    {id:124,name:'raj',salary:70000,city:'Pune'},
+    {id:125,name:'Bob',salary:90000,city:'Chennai'},
+    {id:126,name:'Jay',salary:60000,city:'Delhi'},
+    {id:127,name:'Ali',salary:80000,city:'Banglore'}
+
+  ]
   constructor() { }
 
   ngOnInit() {
