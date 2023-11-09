a8d2a8c7f33cb293d2289949f02dbbfcf2328012
Iamneo <techadmin@iamneo.ai>
2023-11-09 07:18:12 +0000

    Backup Commit

--- demoang/src/app/demo/demo.component.ts
+++ demoang/src/app/demo/demo.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class DemoComponent implements OnInit {
   name:string='Tom'
   age:number=33
-
+  fno:number=90
+  sno:number=30
+  Sum():number{
+    return this.fno+this.sno
+  }
+  Show():void{
+    alert('Name is '+ this.name+'Age is '+this.age)
+  }
   constructor() { }
 
   ngOnInit() {
