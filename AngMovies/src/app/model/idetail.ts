66523c98844e4f24e5c3fcec7ba40c8c364bb4ba
Iamneo <techadmin@iamneo.ai>
2023-11-15 07:45:26 +0000

    Backup Commit

---
+++ AngMovies/src/app/model/idetail.ts
@@ -0,0 +1,10 @@
+export interface IDetail {
+    detailid:number
+    actor:string
+    movieid:number
+    gender:string
+    role:string
+    
+
+
+}
--- AngMovies/src/app/model/imovie.ts
+++ AngMovies/src/app/model/imovie.ts
@@ -1,2 +1,6 @@
 export interface IMovie {
+    id:number
+    name:string
+    yearrelease:number
+    rating:number
 }
