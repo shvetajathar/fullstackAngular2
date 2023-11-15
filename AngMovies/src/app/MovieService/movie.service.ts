497f3b9a2d830b402194e3376c86b9957178488f
Iamneo <techadmin@iamneo.ai>
2023-11-15 08:45:26 +0000

    Backup Commit

---
+++ AngMovies/src/app/MovieService/movie.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { IMovie } from '../model/imovie' ;
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() { }
+}
