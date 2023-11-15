431dbab6fe24a0b4694a3b508dbed03899528274
Iamneo <techadmin@iamneo.ai>
2023-11-15 09:00:26 +0000

    Backup Commit

--- AngMovies/src/app/MovieService/movie.service.ts
+++ AngMovies/src/app/MovieService/movie.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { IMovie } from '../model/imovie' ;
 import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+private url='https://8080-beaddfcabfedbecfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/Movie'
+  constructor(private httpclient:HttpClient) { }
+  getAllMovies():Observable<IMovie[]>
+    return this.httpclient.get<IMovie[]>('${this.url}/listmovies')
 
-  constructor() { }
 }
