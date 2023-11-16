97f946de142d780b3437269b35081fc58bad40da
Iamneo <techadmin@iamneo.ai>
2023-11-16 09:30:30 +0000

    Backup Commit

--- MovieAppAng/src/app/add-movie/add-movie.component.ts
+++ MovieAppAng/src/app/add-movie/add-movie.component.ts
@@ -12,6 +12,15 @@ export class AddMovieComponent implements OnInit {
 moviedata:IMovie={id:0,name:'', yearRelease : 0, rating:0}
 
   constructor(private ms:MovieserviceService,private route:Router) { }
+  saveData(movie:IMovie):void{
+    this.moviedata=movie
+    this.ms.addMovie(this.moviedata).subscribe(
+      ()=>{
+        alert('Record Added Successfully.')
+        this.route.navigate(['/listmovies'])
+      }
+    )
+  }
 
   ngOnInit() {
   }
