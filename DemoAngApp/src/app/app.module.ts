f30917bed9efd96e8543394849a735ff9fb2559f
Iamneo <techadmin@iamneo.ai>
2023-11-18 18:01:14 +0000

    Backup Commit

--- DemoAngApp/src/app/app.module.ts
+++ DemoAngApp/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListmoviesComponent } from './listmovies/listmovies.component';
-
+import { FormsModule,NgForm } from '@angular/forms';
+// import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     ListmoviesComponent
+    // ReactiveFormsModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    // ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
