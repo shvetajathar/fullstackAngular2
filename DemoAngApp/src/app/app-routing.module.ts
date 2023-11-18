67578f0d9ed70a50c5d895e78e8737c64c236a7a
Iamneo <techadmin@iamneo.ai>
2023-11-18 17:46:14 +0000

    Backup Commit

--- DemoAngApp/src/app/app-routing.module.ts
+++ DemoAngApp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MovieService } from './movieservice/movie.service';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'listmovies',component:MovieService}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
