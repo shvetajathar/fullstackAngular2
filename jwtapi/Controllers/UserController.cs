e38a678426ea446302d89be443f2642cf6d14104
Iamneo <techadmin@iamneo.ai>
2023-11-28 09:30:48 +0000

    Backup Commit

--- jwtapi/Controllers/UserController.cs
+++ jwtapi/Controllers/UserController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using jwtapi.Models;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdenitityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
 
 namespace jwtapi.Controllers
 {
@@ -10,9 +17,18 @@ namespace jwtapi.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
-        public iActionResult Register(User u)
+        private readonly AppdContext _authContext;
+        public UserController(AppdContext context)
         {
+            _authContext=context;
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if(userObj==null) return BadRequest();
+            var user=await _authContext.Users.FirstOrDefaultAsync(x=>x.Username==userObj.Username);
 
         }
+        
     }
 }
