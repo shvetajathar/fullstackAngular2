7026cd9d5fda4d0d959bba31455ae55898d511fb
Iamneo <techadmin@iamneo.ai>
2023-11-29 08:52:11 +0000

    Backup Commit

--- jwtclient/src/app/services/auth.service.ts
+++ jwtclient/src/app/services/auth.service.ts
@@ -1,13 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private url="https://8080-beaddfcabfedbecfabcaaaceeafebeccaddbefddaf.premiumproject.examly.io/api/User";
 
-  constructor() { }
+  constructor(private httpclient:HttpClient,private router:Router) { }
   isLoggedIn():boolean
   {
     return false;
+  }
+  signUp(userObj:any)
+  {
+    return  this.httpclient.post<any>(`${this.url}register`,userObj)
+  }
+  signIn(loginObj:any)
+  {
+    return  this.httpclient.post<any>(`${this.url}authenticate`,loginObj)
+    
+  }
+  signOut(){
+    localStorage.clear()
+    this.router.navigate(['login'])
+  }
+  storeToken(tokenValue:string)
+  {
+    localStorage.setItem()
   }
 }
