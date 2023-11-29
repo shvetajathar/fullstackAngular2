4722fcb0970f527271f8e4214bbcd6c2a24bcdbf
Iamneo <techadmin@iamneo.ai>
2023-11-29 04:43:39 +0000

    Backup Commit

--- jwtclient/src/app/app-routing.module.ts
+++ jwtclient/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'dashboard',component:DashboardComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
